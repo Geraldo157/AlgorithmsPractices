91ae89ae2c3065d7f3dd44a6e3cfd85f365c6019
Geraldo157 <gsmecanica76@gmail.com>
2023-04-25 15:03:15 -0300

    Criation of the Repo

---
+++ solutions/LeetCode/FinalValueOfVariable.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        X = 0
+        for i in operations:
+            if  "+" in i: X += 1
+            elif "-" in i: X -= 1
+        return X
