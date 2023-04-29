364072bf63f0ab004942398c38353cc207106675
Geraldo157 <gsmecanica76@gmail.com>
2023-04-29 18:59:06 -0300

    Codewars Solution

---
+++ solutions/CodeWars/MillipedeofWords.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    letras = {}
+    retorno = True
+    for i, v in enumerate(arr):
+        if i[0] not in letras.values():
+            letras[i[0]] = 1
+        else:
+            letras[i[0]] += 1
+        if i[-1] not in letras.values():
+            letras[i[-1]] = 1
+        else:
+            letras[i[-1]] += 1
+    for i in letras.values():
+        if i % 2 == 1:
+            retorno = False
+    return retorno
